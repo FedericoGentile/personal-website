debf8cfebf45dd316cfa7b54b315beab216d1632
Federico Gentile <federicogentile90@gmail.com>
2022-04-25 10:01:50 +0200

    Created button + sidebar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ButtonComponent } from './navbar/button/button.component';
+import { SidebarComponent } from './navbar/sidebar/sidebar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ButtonComponent,
+    SidebarComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/navbar/button/button.component.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit, HostListener } from '@angular/core';
+
+@Component({
+  selector: 'app-button',
+  templateUrl: './button.component.html',
+  styleUrls: ['./button.component.css']
+})
+export class ButtonComponent implements OnInit {
+
+  public symbol: boolean = true;
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  changeSymbol(symbol: boolean): boolean {
+    symbol = !symbol;
+    return symbol;
+  }
+
+  @HostListener("window:resize", []) updateDays() {
+    // lg (for laptops and desktops - screens equal to or greater than 1200px wide)
+    // md (for small laptops - screens equal to or greater than 992px wide)
+    // sm (for tablets - screens equal to or greater than 768px wide)
+    // xs (for phones - screens less than 768px wide)
+  
+    if (window.innerWidth >= 992) {
+      this.symbol = true;//md
+    } 
+  }
+
+}
